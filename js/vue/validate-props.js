cda6e9f7295d0ba0f3ab11ae5e4852cc64acfe15
yeefun <szyeefun@gmail.com>
2021-01-16 22:52:49 +0800

    feat(vue): add validate-props

---
+++ js/vue/validate-props.js
@@ -0,0 +1,17 @@
+/* #validate, #props */
+
+export default {
+  props: {
+    prop1s: {
+      type: Array,
+      required: true,
+      validator(prop1s) {
+        return ['propX', 'propY', 'propZ'].every(function (prop) {
+          return Object.values(prop1s).every(function doeshaveProp(prop1) {
+            return Object.prototype.hasOwnProperty.call(prop1, prop);
+          });
+        });
+      },
+    },
+  },
+};
